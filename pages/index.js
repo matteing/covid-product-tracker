a237f9622ed6b33687d5881faa84d87f4f538b9b
Sergio Mattei <me@sergiomattei.com>
2020-03-15 16:57:15 -0400

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,203 +1,102 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
+const Product = ({ product }) => {
+  return (
+    <div className="flex mb-2 p-4 bg-white rounded-md border border-gray-400  items-center justify-center cursor-pointer">
+      <div className="mr-4">
+        <img className="w-12 h-12" src={product.icon} alt={product.name} />
       </div>
-    </main>
+      <div className="flex-grow">
+        <strong className="text-gray-800">{product.name}</strong>
+        <p className="text-gray-700">{product.description}</p>
+      </div>
+    </div>
+  );
+};
 
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
+const Home = () => {
+  return (
+    <div class="container mx-auto">
+      <div className="flex flex-col min-h-screen">
+        <div className="p-4 pt-8  pb-8 border border-gray-400 border-t-0">
+          <h1>
+            The COVID-19 Product Tracker{" "}
+            <span className="text-sm text-primary">by Makerlog</span>
+          </h1>
+          <h3 className="subtitle mb-4">
+            A directory of tech products aiming to help solve the global
+            pandemic.
+          </h3>
+          <button className="font-base text-md rounded-full block p-2 pl-5 pr-5 bg-white border border-gray-400">
+            Add a product
+          </button>
+        </div>
+        <div className="md:grid md:grid-cols-3  bg-gray-100  flex-grow border border-gray-400 border-t-0 border-b-0">
+          <div className="col-span-2 p-4 ">
+            <div className="title mb-4">
+              <h3 className="font-bold">Remote work tools</h3>
+              <h4 className="subtitle">
+                Go remote with the best of the tech industry
+              </h4>
+            </div>
+            <Product
+              product={{
+                name: "Slack",
+                description: "The best group chat tool for remote teams",
+                icon:
+                  "https://www.stickpng.com/assets/images/5cb480cd5f1b6d3fbadece79.png"
+              }}
+            />
+            <Product
+              product={{
+                name: "Slack",
+                description: "The best group chat tool for remote teams",
+                icon:
+                  "https://www.stickpng.com/assets/images/5cb480cd5f1b6d3fbadece79.png"
+              }}
+            />
+            <Product
+              product={{
+                name: "Slack",
+                description: "The best group chat tool for remote teams",
+                icon:
+                  "https://www.stickpng.com/assets/images/5cb480cd5f1b6d3fbadece79.png"
+              }}
+            />
+            <Product
+              product={{
+                name: "Slack",
+                description: "The best group chat tool for remote teams",
+                icon:
+                  "https://www.stickpng.com/assets/images/5cb480cd5f1b6d3fbadece79.png"
+              }}
+            />
+          </div>
+          <div className="p-4 ">
+            <h3 className="mb-2 font-bold">Categories</h3>
+            <ul>
+              <li className="cursor-pointer p-2 border border-gray-400 border-t-0 border-b-0 border-r-0">
+                Working from home 💻
+              </li>
+              <li className="cursor-pointer p-2 border border-gray-400 border-t-0 border-b-0 border-r-0">
+                Education & resources 📝
+              </li>
+              <li className="cursor-pointer p-2 border border-gray-400 border-t-0 border-b-0 border-r-0">
+                Physical services 🩺
+              </li>
+              <li className="cursor-pointer p-2 border border-gray-400 border-t-0 border-b-0 border-r-0">
+                Indie made 💚
+              </li>
+              <li className="cursor-pointer p-2 border border-gray-400 border-t-0 border-b-0 border-r-0">
+                Publicly built 🔥
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="p-4 border border-gray-400 border-b-0">Hello</div>
+      </div>
+    </div>
+  );
+};
 
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00a676",
+        secondary: "#a2e1cb"
+      }
+      //fontFamily: {
+      //    sans: ["Inter", "sans-serif"]
+      //}
+    }
+  }
+};
