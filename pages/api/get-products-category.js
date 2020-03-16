6efd4fcff51a71e225084d0069c682a089f6e4b3
Sergio Mattei <me@sergiomattei.com>
2020-03-16 02:21:15 -0400

    Fix for category

--- pages/api/get-products-category.js
+++ pages/api/get-products-category.js
@@ -1,11 +1,8 @@
 import { getForCategory } from "../../lib";
-module.exports = async (req, res) => {
-  return res
-    .status(200)
-    .json(
-      await getForCategory(
-        req.query.id,
-        req.query.offset ? req.query.offset : null
-      )
-    );
+export default async (req, res) => {
+  const products = await getForCategory(
+    req.query.id,
+    req.query.offset ? req.query.offset : null
+  );
+  return res.status(200).json(products);
 };
