a75caa68341fdd73999d706191689bdb3058b154
Sergio Mattei <me@sergiomattei.com>
2020-03-16 02:01:42 -0400

    fix purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,15 @@
-// postcss.config.js
 const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
-    // Specify the paths to all of the template files in your project
-    content: [
-      "./**/*.html",
-      "./**/*.vue",
-      "./**/*.jsx",
-      "./**/*.js"
-      // etc.
-    ],
-
-    // Include any special characters you're using in this regular expression
+    content: ["./**/*.js"],
     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
   }
 ];
-
 module.exports = {
   plugins: [
+    "postcss-import",
     "tailwindcss",
     "autoprefixer",
-    process.env.NODE_ENV === "production" ? purgecss : []
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
